8f457dbbee1c7f573d886e454f4dea37a3d94893
kishmat <kishmat17@gmail.com>
2020-08-07 15:32:54 +0545

    Updates

---
+++ precache-manifest.30c6efa25280f0b772a015b67a27f1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa58a4a3404d4fb5437b0d83e7eaab60",
+    "url": "/employee-app/index.html"
+  },
+  {
+    "revision": "c046f6f95d3660c5cfdf",
+    "url": "/employee-app/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "c8b0affada36ae3132aa",
+    "url": "/employee-app/static/css/main.cf783ef7.chunk.css"
+  },
+  {
+    "revision": "c046f6f95d3660c5cfdf",
+    "url": "/employee-app/static/js/2.a2b49920.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-app/static/js/2.a2b49920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b0affada36ae3132aa",
+    "url": "/employee-app/static/js/main.01bda410.chunk.js"
+  },
+  {
+    "revision": "47df681aa57396ed7c1f",
+    "url": "/employee-app/static/js/runtime-main.bef503d9.js"
+  }
+]);
